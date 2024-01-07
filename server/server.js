a3a44f0208d343382dbed9263a3ad75e61b77660
kurylko <reviliya@gmail.com>
2024-01-07 03:10:28 +0100

    back is working

--- server/server.js
+++ server/server.js
@@ -2,10 +2,23 @@ const PORT = process.env.PORT ?? 8000;
 
 const express = require('express');
 const app = express();
+const pool = require('./db');
 
 app.get('/', (req, res) => {
     res.send('hello');
 });
+
+
+// getting all Todos
+app.get('/todos', async (req, res) => {
+   try {
+const todos = await pool.query('SELECT * FROM todos')
+       res.json(todos.rows)
+   } catch (error){
+       console.error('error');
+   }
+});
+
 
 app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`));
 
